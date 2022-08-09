f004dab946f6b06eac6645a8b54c4962e8aba25d
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2022-08-09 10:36:49 +0100

    Using a PopupMenuButton

--- lib/art_route.dart
+++ lib/art_route.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_app/art_util.dart';
 
 class ArtRoute extends StatelessWidget {
   // const ArtRoute({Key? key}) : super(key: key);
 
   final String art;
-  ArtRoute({ required this.art});
+
+  ArtRoute({required this.art});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Navigating art'),
+        title: const Text('Navigating art'),
+        actions: <Widget>[
+          PopupMenuButton(
+              // icon: const Icon(Icons.image),
+              // child: const Text('Change Route'),
+              itemBuilder: (BuildContext context) {
+                return ArtUtil.menuItems
+                    .map(
+                      (String item) => PopupMenuItem(
+                        value: item,
+                        child: Text(item),
+                      ),
+                    )
+                    .toList();
+              },
+            onSelected: (String value) => changeRoute(context, value),
+          ),
+        ],
       ),
       body: Container(
         decoration: BoxDecoration(
@@ -21,5 +40,24 @@ class ArtRoute extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void changeRoute(BuildContext context, String menuItem) {
+    String image = '';
+    switch (menuItem) {
+      case ArtUtil.CARAVAGGIO: {
+        image = ArtUtil.IMG_CARAVAGGIO;
+        break;
+      }
+      case ArtUtil.MONET: {
+        image = ArtUtil.IMG_MONET;
+        break;
+      }
+      case ArtUtil.VANGOGH: {
+        image = ArtUtil.IMG_VANGOGH;
+        break;
+      }
+    }
+    Navigator.push(context, MaterialPageRoute(builder: (context) => ArtRoute(art: image)));
   }
 }
