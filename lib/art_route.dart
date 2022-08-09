0c4533ecc9d93888a685757748e3c76068c03af5
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2022-08-09 11:01:14 +0100

    Using a Drawer

--- lib/art_route.dart
+++ lib/art_route.dart
@@ -11,22 +11,59 @@ class ArtRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                image: DecorationImage(
+                  image: NetworkImage('http://bit.ly/fl_sky'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Text(
+                'Choose your art',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text(ArtUtil.CARAVAGGIO),
+              trailing: const Icon(Icons.art_track),
+              onTap: () => changeRoute(context, ArtUtil.CARAVAGGIO),
+            ),
+            ListTile(
+              title: const Text(ArtUtil.VANGOGH),
+              trailing: const Icon(Icons.art_track),
+              onTap: () => changeRoute(context, ArtUtil.VANGOGH),
+            ),
+            ListTile(
+              title: const Text(ArtUtil.MONET),
+              trailing: const Icon(Icons.art_track),
+              onTap: () => changeRoute(context, ArtUtil.MONET),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: const Text('Navigating art'),
         actions: <Widget>[
           PopupMenuButton(
-              // icon: const Icon(Icons.image),
-              // child: const Text('Change Route'),
-              itemBuilder: (BuildContext context) {
-                return ArtUtil.menuItems
-                    .map(
-                      (String item) => PopupMenuItem(
-                        value: item,
-                        child: Text(item),
-                      ),
-                    )
-                    .toList();
-              },
+            // icon: const Icon(Icons.image),
+            // child: const Text('Change Route'),
+            itemBuilder: (BuildContext context) {
+              return ArtUtil.menuItems
+                  .map(
+                    (String item) => PopupMenuItem(
+                      value: item,
+                      child: Text(item),
+                    ),
+                  )
+                  .toList();
+            },
             onSelected: (String value) => changeRoute(context, value),
           ),
         ],
@@ -45,19 +82,23 @@ class ArtRoute extends StatelessWidget {
   void changeRoute(BuildContext context, String menuItem) {
     String image = '';
     switch (menuItem) {
-      case ArtUtil.CARAVAGGIO: {
-        image = ArtUtil.IMG_CARAVAGGIO;
-        break;
-      }
-      case ArtUtil.MONET: {
-        image = ArtUtil.IMG_MONET;
-        break;
-      }
-      case ArtUtil.VANGOGH: {
-        image = ArtUtil.IMG_VANGOGH;
-        break;
-      }
+      case ArtUtil.CARAVAGGIO:
+        {
+          image = ArtUtil.IMG_CARAVAGGIO;
+          break;
+        }
+      case ArtUtil.MONET:
+        {
+          image = ArtUtil.IMG_MONET;
+          break;
+        }
+      case ArtUtil.VANGOGH:
+        {
+          image = ArtUtil.IMG_VANGOGH;
+          break;
+        }
     }
-    Navigator.push(context, MaterialPageRoute(builder: (context) => ArtRoute(art: image)));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => ArtRoute(art: image)));
   }
 }
